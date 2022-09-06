d758a1bb7fbf77c8ddb200f373451b8d189d833b
Cammy Smith <cjsarab1991@gmail.com>
2022-09-06 12:41:49 +0100

    All tests except 1 complete

--- src/bus_stop.py
+++ src/bus_stop.py
@@ -1,2 +1,15 @@
 class BusStop:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.queuelength = 0
+    
+    def queue_length(self):
+        return self.queuelength
+
+    def add_to_queue(self, person):
+        self.queuelength += 1
+
+    def clear(self):
+        self.queuelength = 0
+
+    
