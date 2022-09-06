595d4754f088f77fac219dac6260eecd4591243d
Cammy Smith <cjsarab1991@gmail.com>
2022-09-06 12:36:33 +0100

    All but one bus tests complete

---
+++ src/bus.py
@@ -0,0 +1,20 @@
+class Bus:
+    def __init__(self, route_number, destination):
+        self.route_number = route_number
+        self.destination = destination
+        self.passengercount = 0
+
+    def drive(self):
+        return "Brum brum"
+    
+    def passenger_count(self):
+        return self.passengercount
+
+    def pick_up(self, person):
+        self.passengercount += 1
+
+    def drop_off(self, person):
+        self.passengercount -= 1
+
+    def empty(self):
+        self.passengercount = 0
